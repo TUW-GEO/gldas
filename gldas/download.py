2cae9e06c45bf8d115b1537769b91c13026a9ead
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2016-07-26 18:17:05 +0200

    Add basic structure of download command line interface.

--- gldas/download.py
+++ gldas/download.py
@@ -1,161 +1,19 @@
 """
-Download latest GLDASv1 and GLDASv2.
-
-@author: Plocon Andreea
-created on: 15.05.2014
+Download GLDAS.
 """
 import os
-import ftplib
+import sys
+import glob
+import argparse
+from functools import partial
 
+import trollsift.parser as parser
 from datetime import datetime
-
-
-def download(host, server_path, raw, download_path, files_per_day=16):
-    """
-    Download latest GLDAS from ftp server.
-
-    Parameters
-    ----------
-    host : str
-        Host name
-    server_path : str
-        Path to data on server.
-    raw : str
-        Path to data local.
-    download_path : str
-        Path where to save the downloaded files.
-    files_per_day: int
-        Optional, number of files in one day. For GLDAS = 16.
-    """
-
-    last = get_last_year_day(raw)
-
-    last_year, last_day = last.split('_')
-    log_file = os.path.join(download_path, 'download_log.txt')
-    downloads = 0
-    status = 'ok'
-
-    # connect to server
-    ftp = ftplib.FTP(host, 'anonymous')
-    # change directory on server
-    ftp.cwd(server_path)
-
-    years = []
-    ftp.retrlines('NLST', years.append)
-
-    for year in sorted(years):
-        try:
-            int(year)
-        except ValueError:
-            continue
-        else:
-            if int(year) >= int(last_year):
-                ftp.cwd(year)
-                days = []
-                ftp.retrlines('NLST', days.append)
-                for day in sorted(days):
-                    if int(day) > int(last_day):
-                        with open(log_file, 'a') as f:
-                            f.write('%s -> Downloading year: %s day:%s \n' %
-                                    (str(datetime.now()), year, day))
-                        day_downloads = 0
-                        ftp.cwd(day)
-                        files2download = []
-                        ftp.retrlines('NLST', files2download.append)
-
-                        # create local directories
-                        year_dir = os.path.join(download_path, year)
-                        if not os.path.exists(year_dir):
-                            os.makedirs(year_dir)
-                        day_dir = os.path.join(year_dir, day)
-                        if not os.path.exists(day_dir):
-                            os.makedirs(day_dir)
-
-                        # download files
-                        for filename in files2download:
-                            downloads += 1
-                            day_downloads += 1
-                            ftp.sendcmd("TYPE i")
-                            size_before = ftp.size(filename)
-
-                            local_filename = os.path.join(day_dir, filename)
-                            local_file = open(local_filename, 'wb')
-                            ftp.retrbinary(
-                                'RETR ' + filename, local_file.write)
-                            local_file.close()
-
-                            size_after = os.path.getsize(local_filename)
-
-                            # check downloaded file
-                            if size_before != size_after:
-                                status = 'wrong'
-                                with open(log_file, 'a') as f:
-                                    f.write('%s -> WARNING: check file %s \n'
-                                            % (str(datetime.now()), local_filename))
-
-                        # check if day is complete
-                        if day_downloads != files_per_day:
-                            status = 'wrong'
-                            with open(log_file, 'a') as f:
-                                f.write('%s -> WARNING: check day %s \n'
-                                        % (str(datetime.now()), day))
-
-                        # return to year directory
-                        ftp.cwd(os.path.join(server_path, year))
-
-                # return to path directory
-                ftp.cwd(server_path)
-
-    ftp.quit()
-
-    with open(log_file, 'a') as f:
-        f.write('%s -> DOWNLOADED %d FILES ' %
-                (str(datetime.now()), downloads))
-        if status == 'ok':
-            f.write('NO WARNINGS!')
-        else:
-            f.write('CHECK WARNINGS!')
-        f.write('\n--------------------------\n')
-
-
-def get_last_year_day(raw_path):
-    """
-    Get last year and day in the raw data.
-
-    Parameters
-    ----------
-    raw_path : str
-        Path to raw data local.
-
-    Returns
-    -------
-    year_day: str
-        String containing last year and last_day
-    """
-
-    last_year = '0'
-    last_day = '0'
-
-    for year in os.walk(raw_path).next()[1]:
-        try:
-            int(year)
-        except ValueError:
-            continue
-        else:
-            if int(year) > int(last_year):
-                last_year = year
-
-    if last_year != '0':
-        for day in os.walk(os.path.join(raw_path, last_year)).next()[1]:
-            try:
-                int(day)
-            except ValueError:
-                continue
-            else:
-                if int(day) > int(last_day):
-                    last_day = day
-
-    return last_year + '_' + last_day
+from datedown.interface import mkdate
+from datedown.dates import n_hourly
+from datedown.urlcreator import create_dt_url
+from datedown.fname_creator import create_dt_fpath
+from datedown.interface import download_by_dt
 
 
 if __name__ == '__main__':
@@ -167,4 +25,155 @@ if __name__ == '__main__':
     raw = '.../raw_GLDAS/'
     download_path = '.../raw_GLDAS/'
 
-    download(host, server_path, raw, download_path, files_per_day=16)
+
+def parse_args(args):
+    """
+    Parse command line parameters for recursive download
+
+    :param args: command line parameters as list of strings
+    :return: command line parameters as :obj:`argparse.Namespace`
+    """
+    parser = argparse.ArgumentParser(
+        description="Download GLDAS data.")
+    parser.add_argument("localroot",
+                        help='Root of local filesystem where the data is stored.')
+    parser.add_argument("-s", "--start", type=mkdate,
+                        help=("Startdate. Either in format YYYY-MM-DD or YYYY-MM-DDTHH:MM."
+                              "If not given then the target folder is scanned for a start date."
+                              "If no data is found there then the first available date of the product is used."))
+    parser.add_argument("-e", "end", type=mkdate,
+                        help=("Enddate. Either in format YYYY-MM-DD or YYYY-MM-DDTHH:MM."
+                              "If not given then the current date is used."))
+    parser.add_argument("--product", choices=["GLDAS_Noah_v1_025"], default="GLDAS_Noah_v1_025",
+                        help='GLDAS product to download.')
+    parser.add_argument("--username",
+                        help='Username to use for download.')
+    parser.add_argument("--password",
+                        help='password to use for download.')
+    parser.add_argument("--n_proc", default=1, type=int,
+                        help='Number of parallel processes to use for downloading.')
+    args = parser.parse_args(args)
+    # set defaults that can not be handled by argparse
+
+    if args.start is None or args.end is None:
+        start, end = gldas_folder_get_start_end(args.localroot)
+        if args.start is None:
+            args.start = start
+        if args.end is None:
+            args.end = end
+
+        return args
+
+def gldas_folder_get_start_end(
+        root,
+        fmt="GLDAS_NOAH025SUBP_3H.A{time:%Y%j.%H%M}.001.{production_time:%Y%j%H%M%S}.grb",
+        subpaths=['{:%Y}', '{:%j}']):
+    """
+    Get first and last product which exists under the root folder.
+
+    Parameters
+    ----------
+    root: string
+        Root folder on local filesystem
+    fmt: string, optional
+        formatting string
+    subpaths: list, optional
+        format of the subdirectories under root.
+
+    Returns
+    -------
+    start: datetime.datetime
+        First found product datetime
+    end: datetime.datetime
+        Last found product datetime
+    """
+    start = None
+    end = None
+    first_folder = get_first_gldas_folder(root, subpaths)
+    last_folder = get_last_gldas_folder(root, subpaths)
+
+    if first_folder is not None:
+        files = sorted(glob.glob(os.path.join(first_folder, parser.globify(fmt))))
+        data = parser.parse(fmt, os.path.split(files[0])[1])
+        start = data['time']
+
+    if last_folder is not None:
+        files = sorted(glob.glob(os.path.join(last_folder, parser.globify(fmt))))
+        data = parser.parse(fmt, os.path.split(files[-1])[1])
+        end = data['time']
+
+    return start, end
+
+
+def get_last_gldas_folder(root, subpaths):
+    directory = root
+    for level, subpath in enumerate(subpaths):
+        last_dir = get_last_formatted_dir_in_dir(directory, subpath)
+        if last_dir is None:
+            directory=None
+            break
+        directory = os.path.join(directory, last_dir)
+    return directory
+
+def get_first_gldas_folder(root, subpaths):
+    directory = root
+    for level, subpath in enumerate(subpaths):
+        last_dir = get_first_formatted_dir_in_dir(directory, subpath)
+        if last_dir is None:
+            directory=None
+            break
+        directory = os.path.join(directory, last_dir)
+    return directory
+
+def get_last_formatted_dir_in_dir(folder, fmt):
+    """
+    Get the (alphabetically) last directory in a directory
+    which can be formatted according to fmt.
+    """
+    last_elem = None
+    root_elements = sorted(os.listdir(folder))
+    for root_element in root_elements[::-1]:
+        if os.path.isdir(os.path.join(folder, root_element)):
+            if parser.validate(fmt, root_element):
+                last_elem = root_element
+                break
+    return last_elem
+
+def get_first_formatted_dir_in_dir(folder, fmt):
+    """
+    Get the (alphabetically) first directory in a directory
+    which can be formatted according to fmt.
+    """
+    first_elem = None
+    root_elements = sorted(os.listdir(folder))
+    for root_element in root_elements:
+        if os.path.isdir(os.path.join(folder, root_element)):
+            if parser.validate(fmt, root_element):
+                first_elem = root_element
+                break
+    return first_elem
+
+def get_gldas_start_date(product):
+    dt_dict = {'GLDAS_Noah_v1_025': datetime(2000,2,24,0)}
+    return dt_dict[product]
+
+def main(args):
+    args = parse_args(args)
+
+    dts = list(n_hourly(args.start, args.end, "3H"))
+    url_create_fn = partial(create_dt_url, root=args.urlroot,
+                            fname='', subdirs=args.urlsubdirs)
+    fname_create_fn = partial(create_dt_fpath, root=args.localroot,
+                              fname='', subdirs=args.localsubdirs)
+    down_func = partial(download,
+                        num_proc=args.n_proc,
+                        username=args.username,
+                        password=args.password,
+                        recursive=True)
+    download_by_dt(dts, url_create_fn,
+                   fname_create_fn, down_func,
+                   recursive=True)
+
+
+def run():
+    main(sys.argv[1:])
---
+++ tests/test_download.py
@@ -0,0 +1,66 @@
+"""
+Tests for the download module of GLDAS.
+"""
+import os
+from datetime import datetime
+from gldas.download import get_last_formatted_dir_in_dir
+from gldas.download import get_first_formatted_dir_in_dir
+from gldas.download import get_last_gldas_folder
+from gldas.download import get_first_gldas_folder
+from gldas.download import gldas_folder_get_start_end
+
+def test_get_last_dir_in_dir():
+    path = os.path.join(os.path.dirname(__file__),
+                        'folder_test', 'success')
+    last_dir = get_last_formatted_dir_in_dir(path, "{:%Y}")
+    assert last_dir == '2014'
+
+def test_get_last_dir_in_dir_failure():
+    path = os.path.join(os.path.dirname(__file__),
+                        'folder_test', 'failure')
+    last_dir = get_last_formatted_dir_in_dir(path, "{:%Y}")
+    assert last_dir == None
+
+
+def test_get_first_dir_in_dir():
+    path = os.path.join(os.path.dirname(__file__),
+                        'folder_test', 'success')
+    last_dir = get_first_formatted_dir_in_dir(path, "{:%Y}")
+    assert last_dir == '2013'
+
+def test_get_last_gldas_folder():
+    path = os.path.join(os.path.dirname(__file__),
+                        'folder_test', 'success')
+    last = get_last_gldas_folder(path, ['{:%Y}', '{:%j}'])
+    last_should = os.path.join(path, "2014", "134")
+    assert last == last_should
+
+def test_get_last_gldas_folder_no_folder():
+    path = os.path.join(os.path.dirname(__file__),
+                        'folder_test', 'failure')
+    last = get_last_gldas_folder(path, ['{:%Y}', '{:%j}'])
+    last_should = None
+    assert last == last_should
+
+def test_get_first_gldas_folder():
+    path = os.path.join(os.path.dirname(__file__),
+                        'folder_test', 'success')
+    last = get_first_gldas_folder(path, ['{:%Y}', '{:%j}'])
+    last_should = os.path.join(path, "2013", "001")
+    assert last == last_should
+
+def test_get_first_gldas_folder_no_folder():
+    path = os.path.join(os.path.dirname(__file__),
+                        'folder_test', 'failure')
+    last = get_first_gldas_folder(path, ['{:%Y}', '{:%j}'])
+    last_should = None
+    assert last == last_should
+
+def test_gldas_get_start_end():
+    path = os.path.join(os.path.dirname(__file__),
+                        'test-data', 'GLDAS_NOAH_image_data')
+    start, end = gldas_folder_get_start_end(path)
+    start_should = datetime(2015,1,1)
+    end_should = datetime(2015,1,1)
+    assert end == end_should
+    assert start == start_should
