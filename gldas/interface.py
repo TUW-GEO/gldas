1cfe62a96fa7cc9d92e12ed412e14c90816a8fcc
Sebastian Hahn <sebastian.hahn@gmail.com>
2018-01-05 23:11:42 +0100

    Fix pep8 issues and update documentation

--- gldas/interface.py
+++ gldas/interface.py
@@ -1,4 +1,24 @@
-﻿# -*- coding: utf-8 -*-
+﻿# The MIT License (MIT)
+#
+# Copyright (c) 2018, TU Wien
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import warnings
 import numpy as np
@@ -19,9 +39,10 @@ from datetime import timedelta
 from gldas.grid import GLDAS025Cellgrid
 from netCDF4 import Dataset
 
+
 class GLDAS_Noah_v21_025Img(ImageBase):
     """
-    Class for reading one GLDAS Noah v2.1 nc file in 0.25° grid.
+    Class for reading one GLDAS Noah v2.1 nc file in 0.25 deg grid.
 
     Parameters
     ----------
@@ -30,96 +51,102 @@ class GLDAS_Noah_v21_025Img(ImageBase):
     mode: string, optional
         mode of opening the file, only 'r' is implemented at the moment
     parameter : string or list, optional
-        one or list of parameters to read, see GLDAS v2.1 documentation for more information
-        Default : 'SoilMoi0_10cm_inst'
+        one or list of parameters to read, see GLDAS v2.1 documentation
+        for more information (default: 'SoilMoi0_10cm_inst').
     array_1D: boolean, optional
-        if set then the data is read into 1D arrays. Needed for some legacy code.
+        if set then the data is read into 1D arrays.
+        Needed for some legacy code.
     """
-    
-    def __init__(self, filename, mode='r', parameter='SoilMoi0_10cm_inst', array_1D=False):
+
+    def __init__(self, filename, mode='r', parameter='SoilMoi0_10cm_inst',
+                 array_1D=False):
+
         super(GLDAS_Noah_v21_025Img, self).__init__(filename, mode=mode)
 
         if type(parameter) != list:
             parameter = [parameter]
+
         self.parameters = parameter
         self.fill_values = np.repeat(9999., 1440 * 120)
         self.grid = GLDAS025Cellgrid()
         self.array_1D = array_1D
-    
+
     def read(self, timestamp=None):
-        
-        #print 'read file: %s' %self.filename
-        #Returns the selected parameters for a gldas image and according metadata
+
+        # print 'read file: %s' %self.filename
+        # Returns the selected parameters for a gldas image and
+        # according metadata
         return_img = {}
         return_metadata = {}
-        
+
         try:
             dataset = Dataset(self.filename)
         except IOError as e:
             print(e)
             print(" ".join([self.filename, "can not be opened"]))
             raise e
-         
 
-        param_names=[]
+        param_names = []
         for parameter in self.parameters:
             param_names.append(parameter)
-            
-        
+
         for parameter, variable in dataset.variables.items():
             if parameter in param_names:
-                param_metadata={}
-                param_data={}           
+                param_metadata = {}
+                param_data = {}
                 for attrname in variable.ncattrs():
-                    if attrname in ['long_name','units']:
-                        param_metadata.update({str(attrname):getattr(variable,attrname)})
-                        
-                   
-                param_data=dataset.variables[parameter][:]
-                np.ma.set_fill_value(param_data,9999)  
-                
-                param_data=np.concatenate((self.fill_values,
-                                           np.ma.getdata(param_data.filled())
-                                           .flatten()))
-                                        
-                return_img.update({str(parameter):param_data[self.grid.activegpis]})
-                return_metadata.update({str(parameter):param_metadata})
-                        
-            #Check for corrupt files
+                    if attrname in ['long_name', 'units']:
+                        param_metadata.update(
+                            {str(attrname): getattr(variable, attrname)})
+
+                param_data = dataset.variables[parameter][:]
+                np.ma.set_fill_value(param_data, 9999)
+
+                param_data = np.concatenate((
+                    self.fill_values,
+                    np.ma.getdata(param_data.filled()) .flatten()))
+
+                return_img.update(
+                    {str(parameter): param_data[self.grid.activegpis]})
+                return_metadata.update({str(parameter): param_metadata})
+
+            # Check for corrupt files
                 try:
                     return_img[parameter]
                 except KeyError:
-                    path, thefile = os.path.split(self.filename)
-                    print ('%s in %s is corrupt - filling image with NaN values' %(parameter,thefile))
-                    return_img[parameter]  = np.empty(self.grid.n_gpi).fill(np.nan)
+                    _, thefile = os.path.split(self.filename)
+                    print('%s in %s is corrupt - filling'
+                          'image with NaN values' % (parameter, thefile))
+                    return_img[parameter] = np.empty(
+                        self.grid.n_gpi).fill(np.nan)
                     return_metadata['corrupt_parameters'].append()
-                    
+
         dataset.close()
+
         if self.array_1D:
-            return Image(self.grid.activearrlon,
-                         self.grid.activearrlat,
-                         return_img,
-                         return_metadata,
-                         timestamp)
+            img = Image(self.grid.activearrlon, self.grid.activearrlat,
+                        return_img, return_metadata, timestamp)
         else:
             for key in return_img:
-                return_img[key]=np.flipud(return_img[key].reshape((720,1440)))
-            
-            return Image(np.flipud(self.grid.activearrlon.reshape((720,1440))),
-                         np.flipud(self.grid.activearrlat.reshape((720,1440))),
-                         return_img, 
-                         return_metadata,
-                         timestamp)
-                        
-                
-    def write(self,data):
+                return_img[key] = np.flipud(
+                    return_img[key].reshape((720, 1440)))
+
+            lons = np.flipud(self.grid.activearrlon.reshape((720, 1440)))
+            lats = np.flipud(self.grid.activearrlat.reshape((720, 1440)))
+
+            img = Image(lons, lats, return_img, return_metadata, timestamp)
+
+        return img
+
+    def write(self, data):
         raise NotImplementedError()
-        
+
     def flush(self):
         pass
-    
+
     def close(self):
         pass
+
 
 class GLDAS_Noah_v1_025Img(ImageBase):
     """
@@ -135,12 +162,14 @@ class GLDAS_Noah_v1_025Img(ImageBase):
         one or list of ['001', '011', '032', '051', '057', '065', '071',
                         '085_L1', '085_L2', '085_L3', '085_L4',
                         '086_L1', '086_L2', '086_L3', '086_L4',
-                        '099', '111', '112', '121', '122', '131', '132', '138',
-                        '155', '204', '205', '234', '235']
+                        '099', '111', '112', '121', '122',
+                        '131', '132', '138', '155',
+                        '204', '205', '234', '235']
         parameters to read, see GLDAS documentation for more information
         Default : '086_L1'
     array_1D: boolean, optional
-        if set then the data is read into 1D arrays. Needed for some legacy code.
+        if set then the data is read into 1D arrays.
+        Needed for some legacy code.
     """
 
     def __init__(self, filename, mode='r', parameter='086_L1', array_1D=False):
@@ -223,12 +252,10 @@ class GLDAS_Noah_v1_025Img(ImageBase):
                 return_img[key] = np.flipud(
                     return_img[key].reshape((720, 1440)))
 
-            return Image(np.flipud(self.grid.activearrlon.reshape((720, 1440))),
-                         np.flipud(
-                             self.grid.activearrlat.reshape((720, 1440))),
-                         return_img,
-                         return_metadata,
-                         timestamp)
+            lons = np.flipud(self.grid.activearrlon.reshape((720, 1440)))
+            lats = np.flipud(self.grid.activearrlat.reshape((720, 1440)))
+
+            return Image(lons, lats, return_img, return_metadata, timestamp)
 
     def write(self, data):
         raise NotImplementedError()
@@ -247,29 +274,28 @@ class GLDAS_Noah_v21_025Ds(MultiTemporalImageBase):
     Parameters
     ----------
     data_path : string
-        path to the nc files
+        Path to the nc files
     parameter : string or list, optional
-        one or list of parameters to read, see GLDAS v2.1 documentation for more information
-        Default : 'SoilMoi0_10cm_inst'
+        One or list of parameters to read, see GLDAS v2.1 documentation
+        for more information. Default : 'SoilMoi0_10cm_inst'
     array_1D: boolean, optional
-        if set then the data is read into 1D arrays. Needed for some legacy code.
+        If set then the data is read into 1D arrays.
+        Needed for some legacy code.
     """
 
-    def __init__(self, data_path, parameter='SoilMoi0_10cm_inst', array_1D=False):
+    def __init__(self, data_path, parameter='SoilMoi0_10cm_inst',
+                 array_1D=False):
 
         ioclass_kws = {'parameter': parameter,
                        'array_1D': array_1D}
 
-        sub_path = ['%Y','%j']
+        sub_path = ['%Y', '%j']
         filename_templ = "GLDAS_NOAH025_3H.A{datetime}.*.nc4"
-        super(GLDAS_Noah_v21_025Ds, self).__init__(data_path, GLDAS_Noah_v21_025Img,
-                                                  fname_templ=filename_templ,
-                                                  datetime_format="%Y%m%d.%H%M",
-                                                  subpath_templ=sub_path,
-                                                  exact_templ=False,
-                                                  ioclass_kws=ioclass_kws)
-        
-    
+
+        super(GLDAS_Noah_v21_025Ds, self).__init__(
+            data_path, GLDAS_Noah_v21_025Img, fname_templ=filename_templ,
+            datetime_format="%Y%m%d.%H%M", subpath_templ=sub_path,
+            exact_templ=False, ioclass_kws=ioclass_kws)
 
     def tstamps_for_daterange(self, start_date, end_date):
         """
@@ -288,14 +314,10 @@ class GLDAS_Noah_v21_025Ds(MultiTemporalImageBase):
             list of datetime objects of each available image between
             start_date and end_date
         """
-        img_offsets = np.array([timedelta(hours=0),
-                                timedelta(hours=3),
-                                timedelta(hours=6),
-                                timedelta(hours=9),
-                                timedelta(hours=12),
-                                timedelta(hours=15),
-                                timedelta(hours=18),
-                                timedelta(hours=21)])
+        img_offsets = np.array([timedelta(hours=0), timedelta(hours=3),
+                                timedelta(hours=6), timedelta(hours=9),
+                                timedelta(hours=12), timedelta(hours=15),
+                                timedelta(hours=18), timedelta(hours=21)])
 
         timestamps = []
         diff = end_date - start_date
@@ -304,6 +326,7 @@ class GLDAS_Noah_v21_025Ds(MultiTemporalImageBase):
             timestamps.extend(daily_dates.tolist())
 
         return timestamps
+
 
 class GLDAS_Noah_v1_025Ds(MultiTemporalImageBase):
     """
@@ -322,7 +345,8 @@ class GLDAS_Noah_v1_025Ds(MultiTemporalImageBase):
         parameters to read, see GLDAS documentation for more information
         Default : '086_L1'
     array_1D: boolean, optional
-        if set then the data is read into 1D arrays. Needed for some legacy code.
+        if set then the data is read into 1D arrays.
+        Needed for some legacy code.
     """
 
     def __init__(self, data_path, parameter='086_L1', array_1D=False):
@@ -332,12 +356,11 @@ class GLDAS_Noah_v1_025Ds(MultiTemporalImageBase):
 
         sub_path = ['%Y', '%j']
         filename_templ = "GLDAS_NOAH025SUBP_3H.A{datetime}.001.*.grb"
-        super(GLDAS_Noah_v1_025Ds, self).__init__(data_path, GLDAS_Noah_v1_025Img,
-                                                  fname_templ=filename_templ,
-                                                  datetime_format="%Y%j.%H%M",
-                                                  subpath_templ=sub_path,
-                                                  exact_templ=False,
-                                                  ioclass_kws=ioclass_kws)
+
+        super(GLDAS_Noah_v1_025Ds, self).__init__(
+            data_path, GLDAS_Noah_v1_025Img, fname_templ=filename_templ,
+            datetime_format="%Y%j.%H%M", subpath_templ=sub_path,
+            exact_templ=False, ioclass_kws=ioclass_kws)
 
     def tstamps_for_daterange(self, start_date, end_date):
         """
@@ -356,14 +379,10 @@ class GLDAS_Noah_v1_025Ds(MultiTemporalImageBase):
             list of datetime objects of each available image between
             start_date and end_date
         """
-        img_offsets = np.array([timedelta(hours=0),
-                                timedelta(hours=3),
-                                timedelta(hours=6),
-                                timedelta(hours=9),
-                                timedelta(hours=12),
-                                timedelta(hours=15),
-                                timedelta(hours=18),
-                                timedelta(hours=21)])
+        img_offsets = np.array([timedelta(hours=0), timedelta(hours=3),
+                                timedelta(hours=6), timedelta(hours=9),
+                                timedelta(hours=12), timedelta(hours=15),
+                                timedelta(hours=18), timedelta(hours=21)])
 
         timestamps = []
         diff = end_date - start_date
