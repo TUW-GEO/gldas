cbbd700e84c1feaa35f42e07ef840d1c00d4dd04
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2021-03-08 15:52:59 +0100

    GLDAS EP, pyscaffold 4, GH actions (#34)
    * Install pygrib via conda
    
    * Fix folder detection for new trollsift version
    
    * Update travis install
    
    * Update CI
    
    * Update README.rst
    
    * Exclude pytest v3.9.1
    
    * Add coveragerc
    
    * Update test-requirements.txt
    
    * include varnames
    
    * Add gitattributes and update coveragerc
    
    * Update environment.yml
    
    * Add support for GLDAS EP
    
    * Fix pep8 formatting
    
    * Clean up
    
    * Update test to always use bbox
    
    * Update pyscaffold
    
    * Add CI workflow
    
    * Reduce builds for now

--- tests/test_download.py
+++ tests/test_download.py
@@ -9,73 +9,62 @@ from gldas.download import get_last_gldas_folder
 from gldas.download import get_first_gldas_folder
 from gldas.download import gldas_folder_get_version_first_last
 
+
 def test_get_last_dir_in_dir():
-    path = os.path.join(os.path.dirname(__file__),
-                        'folder_test', 'success')
+    path = os.path.join(os.path.dirname(__file__), "folder_test", "success")
     last_dir = get_last_formatted_dir_in_dir(path, "{time:%Y}")
-    assert last_dir == '2014'
+    assert last_dir == "2014"
+
 
 def test_get_last_dir_in_dir_failure():
-    path = os.path.join(os.path.dirname(__file__),
-                        'folder_test', 'failure')
+    path = os.path.join(os.path.dirname(__file__), "folder_test", "failure")
     last_dir = get_last_formatted_dir_in_dir(path, "{time:%Y}")
     assert last_dir == None
 
 
 def test_get_first_dir_in_dir():
-    path = os.path.join(os.path.dirname(__file__),
-                        'folder_test', 'success')
+    path = os.path.join(os.path.dirname(__file__), "folder_test", "success")
     last_dir = get_first_formatted_dir_in_dir(path, "{time:%Y}")
-    assert last_dir == '2013'
+    assert last_dir == "2013"
+
 
 def test_get_last_gldas_folder():
-    path = os.path.join(os.path.dirname(__file__),
-                        'folder_test', 'success')
-    last = get_last_gldas_folder(path, ['{time:%Y}', '{time:%j}'])
+    path = os.path.join(os.path.dirname(__file__), "folder_test", "success")
+    last = get_last_gldas_folder(path, ["{time:%Y}", "{time:%j}"])
     last_should = os.path.join(path, "2014", "134")
     assert last == last_should
 
+
 def test_get_last_gldas_folder_no_folder():
-    path = os.path.join(os.path.dirname(__file__),
-                        'folder_test', 'failure')
-    last = get_last_gldas_folder(path, ['{time:%Y}', '{time:%j}'])
+    path = os.path.join(os.path.dirname(__file__), "folder_test", "failure")
+    last = get_last_gldas_folder(path, ["{time:%Y}", "{time:%j}"])
     last_should = None
     assert last == last_should
 
+
 def test_get_first_gldas_folder():
-    path = os.path.join(os.path.dirname(__file__),
-                        'folder_test', 'success')
-    last = get_first_gldas_folder(path, ['{time:%Y}', '{time:%j}'])
+    path = os.path.join(os.path.dirname(__file__), "folder_test", "success")
+    last = get_first_gldas_folder(path, ["{time:%Y}", "{time:%j}"])
     last_should = os.path.join(path, "2013", "001")
     assert last == last_should
 
+
 def test_get_first_gldas_folder_no_folder():
-    path = os.path.join(os.path.dirname(__file__),
-                        'folder_test', 'failure')
-    last = get_first_gldas_folder(path, ['{time:%Y}', '{time:%j}'])
+    path = os.path.join(os.path.dirname(__file__), "folder_test", "failure")
+    last = get_first_gldas_folder(path, ["{time:%Y}", "{time:%j}"])
     last_should = None
     assert last == last_should
 
+
 def test_gldas_get_start_end():
-    path = os.path.join(os.path.dirname(__file__),
-                        'test-data', 'GLDAS_NOAH_image_data')
+    path = os.path.join(
+        os.path.dirname(__file__), "test-data", "GLDAS_NOAH_image_data"
+    )
     version, start, end = gldas_folder_get_version_first_last(path)
-    version_should='GLDAS_Noah_v21_025'
-    start_should = datetime(2015,1,1)
-    end_should = datetime(2015,1,1)
+    version_should = "GLDAS_Noah_v21_025"
+    start_should = datetime(2015, 1, 1)
+    end_should = datetime(2015, 1, 1)
     assert version == version_should
     assert end == end_should
     assert start == start_should
-    
 
-
-if __name__ == '__main__':
-    test_gldas_get_start_end()
-    test_get_last_dir_in_dir()
-    test_get_first_dir_in_dir()
-    test_get_first_gldas_folder()
-    test_get_first_gldas_folder_no_folder()
-    test_get_last_dir_in_dir()
-    test_get_last_gldas_folder()
-    test_get_last_gldas_folder_no_folder()
-    test_get_last_dir_in_dir_failure()
